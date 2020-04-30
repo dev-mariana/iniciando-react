41569fa13623efa89d2223f097379eaf5183b797
Mariana Bastos Rodrigues <m.bastos@rioanalytics.com.br>
2020-04-30 14:23:07 -0300

    primeiros passos com react

---
+++ src/componentes/Multiplos.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+// criando múltiplos components
+
+export const BoaTarde = props => <h1>Boa tarde {props.nome}!</h1>
+
+export const BoaNoite = props => <h1>Boa noite {props.nome}!</h1>
+
+//export { BoaTarde, BoaNoite }
+export default { BoaTarde, BoaNoite }
---
+++ src/componentes/Primeiro.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+// criando o primeiro component
+
+export default () => <h1>Primeiro</h1>
+
+/* quando um component for criado por você, colocar sempre o nome com letra maiuscula, 
+é um padrão do react, senão não é reconhecido. */
